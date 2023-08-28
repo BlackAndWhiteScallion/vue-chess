a185a60bd26e4a8da92c8854422e85ebec979c7c
BlackAndWhiteScallion <tanguo@hotmail.com>
2023-08-28 16:17:31 +0800

    update vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,11 @@
-<script>
-import {reactive} from 'vue';
-import { TheChessboard } from 'vue3-chessboard'; 
-import 'vue3-chessboard/style.css'
-
-const boardConfig = reactive({
-  coordinates: true,
-  autoCastle: false,
-  orientation: 'white',
-});
-
-let boardAPI;
-
-export default{
-  data: function(){
-    return {
-      history:''
-    }
-  },
-  components:{
-    TheChessboard
-  },
-  methods:{
-    updateHistory(){
-      this.history = this.boardAPI.getPossibleMoves();
-    },
-    showThreats(){
-      let moves = this.boardAPI.getPossibleMoves();
-      
-    }
-  }
-}
-
+<script setup>
 </script>
 
 <template>
-  <main>
-    <body>
-      <div class="left-container">
-        <div class="board-container">
-          <TheChessboard :board-config="boardConfig" @board-created="(api) => (boardAPI = api)" @move="updateHistory()"/>
-        </div>
-        <button @click="boardAPI.undoLastMove()">Undo</button>
-        <button @click="boardConfig.orientation=(boardConfig.orientation == 'white'?'black':'white')">Flip Board</button>
-        <button @click="showThreats()">Show Threats</button>
-      </div>
-      <div>
-        <p>{{ history }}</p>
-      </div>
-    </body>
-  </main>
+  <div>
+    <router-view />
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-body{
-  display: flex;
-  align-content: space-evenly;
-}
-
-.container{
-  width: 100%;
-}
-
-.main-wrap{
-  width: 400px;  
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
---
+++ src/components/SideNav.vue
@@ -0,0 +1,30 @@
+<template>
+
+</template>
+
+<style>
+.collapse-enter-active,
+.collapse-leave-active {
+    transition: all 0.2s;
+}
+.collapse-enter,
+.collapse-leave-to {
+    opacity: 0;
+    transform: translateX(-100%);
+}
+</style>
+
+<script>
+export default{
+    data(){
+        return{
+            isCollapse:false
+        }
+    },
+    methods:{
+        toggleCollapse(){
+            this.isCollapse = !this.isCollapse;
+        }
+    }
+}
+</script>
