cac83f7b2c816bff29706b6cbf00bd6fba3d3e43
BlackAndWhiteScallion <tanguo@hotmail.com>
2023-08-31 13:38:16 +0800

    sidenav redo
    i want it to collapse on hover dammit

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -1,24 +1,41 @@
 <template>
-    <el-container>
-        <el-menu
-            default-active="2"
-            class="el-menu-vertical-demo"
-            :collapse="isCollapse"
-        >
-        <router-link to="/">
-          <el-menu-item index="1">
-          <el-icon><icon-menu /></el-icon>
-          <template #title>Navigator Two</template>
-          </el-menu-item>
-        </router-link>
-      <router-link to="/about">
-        <el-menu-item index="3">
-          <el-icon><setting /></el-icon>
-          <template #title>Navigator Four</template>
-        </el-menu-item> 
-      </router-link>
+  <el-menu
+    default-active="2"
+    class="el-menu-vertical-demo"
+    :collapse="isCollapse"
+    @hover="handleHover"
+  >
+    <el-sub-menu index="1">
+      <template #title>
+        <el-icon><location /></el-icon>
+        <span>Navigator One</span>
+      </template>
+      <el-menu-item-group>
+        <template #title><span>Group One</span></template>
+        <el-menu-item index="1-1">item one</el-menu-item>
+        <el-menu-item index="1-2">item two</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="Group Two">
+        <el-menu-item index="1-3">item three</el-menu-item>
+      </el-menu-item-group>
+      <el-sub-menu index="1-4">
+        <template #title><span>item four</span></template>
+        <el-menu-item index="1-4-1">item one</el-menu-item>
+      </el-sub-menu>
+    </el-sub-menu>
+    <el-menu-item index="2">
+      <el-icon><icon-menu /></el-icon>
+      <template #title>Navigator Two</template>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <el-icon><document /></el-icon>
+      <template #title>Navigator Three</template>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <el-icon><setting /></el-icon>
+      <template #title>Navigator Four</template>
+    </el-menu-item>
   </el-menu>
-    </el-container>
 </template>
 
 <style scoped>
@@ -49,5 +66,9 @@ import {
   Setting,
 } from '@element-plus/icons-vue'
 
-const isCollapse = ref(true)
+const isCollapse = ref(true);
+
+const handleHover = function(){
+  this.isCollapse=!isCollapse;
+}
 </script>
